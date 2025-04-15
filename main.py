74a258777c90d79c06eae58ac577e24837240284
BetaCat <23499622+BetaCatX@users.noreply.github.com>
2025-04-15 17:14:05 +0800

    更新 Gotify 插件，添加 chat_id 处理和配置保存功能

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from astrbot.core.message.message_event_result import MessageChain
 class MyPlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.config = config
         self.context = context
         self.server = config.get("server")
         self.token = config.get("token")
@@ -81,20 +82,16 @@ class MyPlugin(Star):
                 await asyncio.sleep(60)  # 等待 1 分钟后重连
         pass
 
-    # 注册指令的装饰器。指令名为 gotify_push。注册成功后，发送 `/gotify_push` 就会触发这个指令
-    @filter.command("gotify_push")
+    @filter.command("gotify_register")
     async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        logger.info(event.unified_msg_origin)
-        message_str = event.message_str  # 用户发的纯文本消息字符串
-        message_chain = (
-            event.get_messages()
-        )  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(
-            f"Hello, {user_name}, 你发了 {message_str}!"
-        )  # 发送一条纯文本消息
+        logger.info(f"当前的chat_id:{event.unified_msg_origin}")
+        self.chat_id.append(event.unified_msg_origin)
+        self.chat_id = list(set(self.chat_id))  # 去重
+        logger.info(f"当前的chat_id:{self.chat_id}")
+        self.config["chat_id"] = self.chat_id
+        self.config.save_config()
+        logger.info(f"当前config:{self.config}，已保存到配置文件中")
+        yield event.stop_event()
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
